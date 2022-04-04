0fcc3db0b7b6cd23900122100f6de61f6999a4f4
zDeadNow123 <dinnuhgamer@gmail.com>
2022-04-04 13:53:13 -0300

    Tudo Funcionando

--- apostassystem/Program.cs
+++ apostassystem/Program.cs
@@ -6,14 +6,17 @@ using System.Threading.Tasks;
 
 namespace apostassystem {
     internal class Program {
-        static void Main(string[] args) {
 
-            apostas();
+        string[] cartoes;
+        public static void Main(string[] args) {
+
+            Program p = new Program();
+            p.apostas();
 
             Console.ReadKey();
         }
 
-        public static void apostas() {
+        public void apostas() {
             // Sistema de apostas
 
             // Usuário solicita o numero de cartões desejados
@@ -21,7 +24,7 @@ namespace apostassystem {
             int numCartoes = int.Parse(Console.ReadLine());
 
             // Criar cartões para apostas com base no numero de cartões solicitados, cada cartão com 10 numeros aleatorios de 1 a 60, divididos em 5 grupos por "-", e cada grupo com 2 numeros aleatorios
-            string[] cartoes = new string[numCartoes];
+            cartoes = new string[numCartoes];
 
             for (int i = 0; i < numCartoes; i++) {
 
@@ -49,6 +52,22 @@ namespace apostassystem {
                 Console.WriteLine("Cartão " + (i + 1) + ": " + cartoes[i]);
             }
 
+            changeCard:
+            // Perguntar ao usuário se ele deseja gerar um numero aleatorio para um cartão especifico
+            Console.WriteLine("Deseja gerar um numero aleatorio para um cartão especifico? (s/n)");
+            string resposta = Console.ReadLine();
+
+            Console.Clear();
+
+            if (resposta == "S" || resposta == "s") {
+                Console.WriteLine("Digite o numero do cartao desejado:");
+                int cartao = int.Parse(Console.ReadLine());
+
+                // Chama o método que gera o numero aleatorio para o cartao
+                changeCard(cartao, numCartoes);
+                goto changeCard; // Depois que o método encerra ele torna a perguntar ao usuário se ele deseja gerar um numero aleatorio para um cartão especifico
+            }
+
             // Perguntar ao usuário se deseja voltar ao inicio do programa
             Console.WriteLine("Deseja voltar ao inicio do programa? (S/N)");
             string resposta2 = Console.ReadLine();
@@ -57,6 +76,37 @@ namespace apostassystem {
                 Console.Clear();
                 apostas();
             }
+
+        }
+
+        public void changeCard(int cartao, int numerocartoes) {
+
+                Console.Clear();
+                
+                string card = "";
+
+                int[] Groups = new int[5];
+
+                for (int j = 0; j < 5; j++) {
+
+                    Groups[j] = JRandom.Range(1, 61);
+
+                    // Verificar se o numero aleatorio gerado ja existe no cartao
+                    while (card.Contains(Groups[j].ToString())) {
+                        Groups[j] = JRandom.Range(1, 61);
+                    }
+
+                    card = string.Join("-", Groups);
+                }
+
+                
+                for (int i = 0; i < numerocartoes; i++) {
+
+                    cartoes[cartao - 1] = card;
+
+                    Console.WriteLine("Cartão " + (i + 1) + ": " + cartoes[i]);
+                }
+                
 
         }
     }
